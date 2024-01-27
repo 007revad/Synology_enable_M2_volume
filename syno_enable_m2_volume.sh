b23a5b9e1df5a5b666b3ea52f37e6d511b598850
Dave Russell <39733752+007revad@users.noreply.github.com>
2024-01-28 05:35:06 +1100

    Update syno_enable_m2_volume.sh

--- syno_enable_m2_volume.sh
+++ syno_enable_m2_volume.sh
@@ -11,7 +11,7 @@
 # sudo /volume1/scripts/syno_enable_m2_volume.sh
 #------------------------------------------------------------------------------
 
-scriptver="v1.1.17"
+scriptver="v1.1.18"
 script=Synology_enable_M2_volume
 repo="007revad/Synology_enable_M2_volume"
 scriptname=syno_enable_m2_volume
